0394a56bf524276d22ece9a40681774a2f083b46
Seekingdefeat <1946877869@qq.com>
2022-08-16 22:18:30 +0800

    登陆界面

---
+++ app/src/main/java/com/zhj/myapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zhj.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_layout)
+    }
+}
