dbfc21de37b5e7f0affecdd298c0f47d5cb9a491
Seekingdefeat <1946877869@qq.com>
2022-08-16 23:46:23 +0800

    Register commit

--- app/src/main/java/com/zhj/myapp/MainActivity.kt
+++ app/src/main/java/com/zhj/myapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.zhj.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zhj.myapp.ui.activity.LoginActivity
+import com.zhj.myapp.ui.activity.RegisterActivity
+import kotlinx.android.synthetic.main.login_layout.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_layout)
+
+        btn_login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        btn_register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
